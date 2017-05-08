28cc7dde62596690ef6b0824483592c2c6145f39
christianwoll <christianwoll.1@gmail.com>
2017-05-08 12:53:57 -0700

    more waveforms

--- tone.py
+++ tone.py
@@ -10,11 +10,15 @@ fs = 44100
 sd.default.samplerate = fs 
 sd.default.channels = 1
 
-def tone(freq, duration):
+def wave(freq, duration, volume=1, form=None, decay=1):
+    if form is None: form = lambda x: np.sin(2*np.pi * x)
+    if freq is None: return np.zeros(int(duration * fs))
+
     period = int(fs / freq)
-    wave = [n / period for n in range(period)]
-    tone = wave * int(freq * duration + 1)
-    return tone[:int(duration * fs)]
+    single = np.fromfunction(lambda i: form(i/period), (period,))
+    tone = np.tile(single, int(freq * duration + 1))
+
+    return tone
     
 def play(wf):
     sd.play(wf)
@@ -23,10 +27,9 @@ def play(wf):
 
 key = [261.625*2**(n/12) for n in range(12)]
 
-def seq(notes, beat):
-    sound = []
-    for n in notes: sound += tone(key[n%12]*2**(n//12), beat)
-    return sound
+def seq(notes, beat, form=None):
+    freqs = [None if n is None else key[n%12]*2**(n//12) for n in notes]
+    return np.concatenate([wave(f, beat, form) for f in freqs])
 
 def chord(notes, beat):
     return merge(*[seq([n], beat) for n in notes])
@@ -36,9 +39,12 @@ def merge(*wf_arr):
     WF_arr = [wf + [0.0] * (M - len(wf)) for wf in wf_arr]
     return [np.mean(v) for v in zip(*WF_arr)]
     
-beat = 0.4
+beat = 0.2
+song = np.array([])
 
-har = seq([10,7]*4 + [9,5]*4 + [7,4]*4 + [5,2]*4, beat)
-har += seq([10,7]*4 + [14,9]*4 + [12,7]*4 + [10,5]*4, beat)
-
-play(har)
+song = np.concatenate((song, seq([6,8,None,10,8,None,6,8], beat)))
+song = np.concatenate((song, seq([5,6,None,10,6,None,5,6], beat)))
+song = np.concatenate((song, seq([3,6,None,10,3,None,5,3], beat)))
+song = np.concatenate((song, seq([11,10,None,6,10,None,6,11], beat)))
+song = np.tile(song, 4)
+play(song)
